83420af87c711e9e8066392fa8550f7c67b6ece0
rio.sung.ks@gmail.com <rio.sung.ks@gmail.com>
2025-06-05 11:23:05 +0900

    Chore: npm package update

--- ios/kamera/AppDelegate.swift
+++ ios/kamera/AppDelegate.swift
@@ -1,48 +1,37 @@
 import UIKit
 import React
-import React_RCTAppDelegate
-import ReactAppDependencyProvider
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
+  
   var window: UIWindow?
-
-  var reactNativeDelegate: ReactNativeDelegate?
-  var reactNativeFactory: RCTReactNativeFactory?
+  var bridge: RCTBridge?
+  var rootView: RCTRootView?
 
   func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil
   ) -> Bool {
-    let delegate = ReactNativeDelegate()
-    let factory = RCTReactNativeFactory(delegate: delegate)
-    delegate.dependencyProvider = RCTAppDependencyProvider()
+    let jsBundleURL: URL = {
+      #if DEBUG
+      return RCTBundleURLProvider.sharedSettings()
+        .jsBundleURL(forBundleRoot: "index", fallbackExtension: nil)!
+      #else
+      return Bundle.main.url(forResource: "main", withExtension: "jsbundle")!
+      #endif
+    }()
 
-    reactNativeDelegate = delegate
-    reactNativeFactory = factory
+    bridge = RCTBridge(bundleURL: jsBundleURL, moduleProvider: nil, launchOptions: launchOptions)
+
+    rootView = RCTRootView(bridge: bridge!, moduleName: "kamera", initialProperties: nil)
+    rootView?.backgroundColor = .white
 
     window = UIWindow(frame: UIScreen.main.bounds)
-
-    factory.startReactNative(
-      withModuleName: "kamera",
-      in: window,
-      launchOptions: launchOptions
-    )
+    let rootViewController = UIViewController()
+    rootViewController.view = rootView
+    window?.rootViewController = rootViewController
+    window?.makeKeyAndVisible()
 
     return true
-  }
-}
-
-class ReactNativeDelegate: RCTDefaultReactNativeFactoryDelegate {
-  override func sourceURL(for bridge: RCTBridge) -> URL? {
-    self.bundleURL()
-  }
-
-  override func bundleURL() -> URL? {
-#if DEBUG
-    RCTBundleURLProvider.sharedSettings().jsBundleURL(forBundleRoot: "index")
-#else
-    Bundle.main.url(forResource: "main", withExtension: "jsbundle")
-#endif
   }
 }
