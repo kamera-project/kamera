db49d9d818f55e6cb0e5d08a15c0e6dbd333db00
Hyeseong <sdgoh0905@gmail.com>
2025-06-09 22:49:18 +0900

    Feat: add camera switch between front and back

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -3,14 +3,20 @@ import { StyleSheet, TouchableOpacity, View } from "react-native";
 
 import FlashToggleIcon from "../../assets/svg/flash-auto.svg";
 import FlipCameraIcon from "../../assets/svg/flip-camera.svg";
+import { useCameraDevice } from "react-native-vision-camera";
+import { useCameraStore } from "../../store/useCameraStore";
 
 export default function CameraHeader() {
+  const backCamera = useCameraDevice('back');
+  const frontCamera = useCameraDevice('front');
+  const chosenDevice = useCameraStore((state) => state.chosenDevice);
+  const setChosenDevice = useCameraStore((state) => state.setChosenDevice);
 
   const onPressFlash = () => {
     console.log('FlashToggleIcon');
   }
-  const onPressCamera = () => {
-    console.log('FlipCameraIcon');
+  const onToggleCamera = () => {
+    setChosenDevice(chosenDevice.position === 'back' ? frontCamera : backCamera);
   }
 
   return (
@@ -18,7 +24,7 @@ export default function CameraHeader() {
       <TouchableOpacity onPress={onPressFlash} style={styles.icon}>
         <FlashToggleIcon width={40} height={40} />
       </TouchableOpacity>
-      <TouchableOpacity onPress={onPressCamera} style={styles.icon}>
+      <TouchableOpacity onPress={onToggleCamera} style={styles.icon}>
         <FlipCameraIcon width={40} height={40} />
       </TouchableOpacity>
     </View>
