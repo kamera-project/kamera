4b3be0485e23a5bc61afc05b81a5e6702a1e74fa
Hyeseong Oh <161146017+HyeseongO@users.noreply.github.com>
2025-06-16 19:22:44 +0900

    Fix: prevent crash when initial camera permission request is denied (close #12)
    * fix: prevent crash when initial camera permission request is denied
    
    * fix: including 'restricated' in camera permission
    
    * fix: change 'null' to 'undefined' in camera permission

--- src/screens/CameraScreen.jsx
+++ src/screens/CameraScreen.jsx
@@ -10,6 +10,8 @@ import {
   Modal,
   TouchableOpacity,
   ScrollView,
+  Alert,
+  Linking,
 } from 'react-native';
 
 import { Camera, useCameraDevice } from 'react-native-vision-camera';
@@ -243,9 +245,26 @@ export default function CameraScreen() {
     </Modal>
   );
 
+  useEffect(() => {
+    if (cameraPermission === 'denied') {
+      Alert.alert(
+        '권한이 필요합니다',
+        '카메라/마이크 권한이 꺼져 있어 기능을 사용할 수 없습니다. 설정에서 권한을 허용해주세요.',
+        [
+          { text: '취소', style: 'cancel' },
+          {
+            text: '설정으로 이동',
+            onPress: () => {
+              Linking.openURL('app-settings:');
+            },
+          },
+        ],
+      );
+    }
+  }, [cameraPermission]);
+
   if (
-    cameraPermission === null ||
-    cameraPermission === 'denied' ||
+    cameraPermission === undefined ||
     cameraPermission === 'not-determined'
   ) {
     return (
@@ -257,6 +276,16 @@ export default function CameraScreen() {
           }
           onPress={requestPermissions}
           disabled={isRequesting}
+        />
+      </View>
+    );
+  } else if (cameraPermission === 'denied' || cameraPermission === 'restricted') {
+    return (
+      <View style={styles.centerPosition}>
+        <Text style={styles.titleText}>설정에서 권한을 허용해주세요.</Text>
+        <Button
+          title="설정 열기"
+          onPress={() => Linking.openURL('app-settings:')}
         />
       </View>
     );
@@ -273,7 +302,7 @@ export default function CameraScreen() {
         } else {
         }
       }, 100);
-    } catch (err) {}
+    } catch (err) { }
   }
 
   const onWebViewMessage = (event) => {
