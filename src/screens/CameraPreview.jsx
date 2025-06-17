8da3f8dae11f649e225f4590a127f69e0c4991f3
rio.sung.ks@gmail.com <rio.sung.ks@gmail.com>
2025-06-17 13:19:11 +0900

    design: re-arrange a reset button

--- src/screens/CameraPreview.jsx
+++ src/screens/CameraPreview.jsx
@@ -51,7 +51,8 @@ export default function CameraPreview() {
 
   const backCamera = useCameraDevice('back');
   const frontCamera = useCameraDevice('front');
-  const { photoPermissionStatus, requestGalleryPermissions, openAppSettings } = usePhotoPermission();
+  const { photoPermissionStatus, requestGalleryPermissions, openAppSettings } =
+    usePhotoPermission();
   const [isBottomSheetVisible, setIsBottomSheetVisible] = useState(false);
   const bottomSheetHeight = useRef(new Animated.Value(0)).current;
   const initialCameraMode = backCamera || frontCamera;
@@ -307,7 +308,7 @@ export default function CameraPreview() {
         } else {
         }
       }, 100);
-    } catch (err) { }
+    } catch (err) {}
   }
 
   const onWebViewMessage = (event) => {
@@ -328,16 +329,23 @@ export default function CameraPreview() {
   };
 
   const openGallery = async () => {
-    if (photoPermissionStatus === 'granted' || photoPermissionStatus === 'limited') {
+    if (
+      photoPermissionStatus === 'granted' ||
+      photoPermissionStatus === 'limited'
+    ) {
       setIsGalleryVisible(true);
-    } else if (photoPermissionStatus === 'denied' || photoPermissionStatus === 'blocked') {
+    } else if (
+      photoPermissionStatus === 'denied' ||
+      photoPermissionStatus === 'blocked'
+    ) {
       Alert.alert('사진 접근 권한이 필요합니다', '설정에서 허용해주세요', [
         { text: '취소', style: 'cancel' },
         { text: '설정 열기', onPress: openAppSettings },
       ]);
     } else {
       const requestResult = await requestGalleryPermissions();
-      if (requestResult === 'granted' || requestResult === 'limited') setIsGalleryVisible(true);
+      if (requestResult === 'granted' || requestResult === 'limited')
+        setIsGalleryVisible(true);
       else if (requestResult === 'denied' || requestResult === 'blocked') {
         Alert.alert('사진 접근 권한이 필요합니다', '설정에서 허용해주세요', [
           { text: '취소', style: 'cancel' },
@@ -412,11 +420,16 @@ export default function CameraPreview() {
         )}
         {transparentOverlay && (
           <View style={styles.resetButtonContainer}>
-            <Button
+            <Svg.ResetOverlay
+              width={50}
+              height={50}
+              onPress={resetPhoto}
+            />
+            {/* <Button
               title='RESET'
               color='#FFF'
               onPress={resetPhoto}
-            />
+            /> */}
           </View>
         )}
       </View>
@@ -470,8 +483,8 @@ const styles = StyleSheet.create({
   },
   resetButtonContainer: {
     position: 'absolute',
-    bottom: 0,
-    right: SCREEN_WIDTH * 0.05,
+    // bottom: 0,
+    right: -10,
     paddingVertical: SCREEN_HEIGHT * 0.015,
     paddingHorizontal: SCREEN_WIDTH * 0.04,
     borderRadius: 6,
