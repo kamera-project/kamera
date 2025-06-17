dd8db8f5ffe868bda0404bb0468ac77e4504f736
rio.sung.ks@gmail.com <rio.sung.ks@gmail.com>
2025-06-17 11:55:00 +0900

    refactor: call a thumbnail variable from store

--- src/screens/CameraPreview.jsx
+++ src/screens/CameraPreview.jsx
@@ -46,12 +46,12 @@ export default function CameraPreview() {
 
   const isRequesting = useCameraStore((state) => state.isRequesting);
   const setIsRequesting = useCameraStore((state) => state.setIsRequesting);
-  const thumbnailUri = useCameraStore((state) => state.thumbnailUri);
   const getLatestPhoto = useCameraStore((state) => state.getLatestPhoto);
 
   const backCamera = useCameraDevice('back');
   const frontCamera = useCameraDevice('front');
-  const { photoPermissionStatus, requestGalleryPermissions, openAppSettings } = usePhotoPermission();
+  const { photoPermissionStatus, requestGalleryPermissions, openAppSettings } =
+    usePhotoPermission();
   const [isBottomSheetVisible, setIsBottomSheetVisible] = useState(false);
   const bottomSheetHeight = useRef(new Animated.Value(0)).current;
   const initialCameraMode = backCamera || frontCamera;
@@ -307,7 +307,7 @@ export default function CameraPreview() {
         } else {
         }
       }, 100);
-    } catch (err) { }
+    } catch (err) {}
   }
 
   const onWebViewMessage = (event) => {
@@ -328,16 +328,23 @@ export default function CameraPreview() {
   };
 
   const openGallery = async () => {
-    if (photoPermissionStatus === 'granted' || photoPermissionStatus === 'limited') {
+    if (
+      photoPermissionStatus === 'granted' ||
+      photoPermissionStatus === 'limited'
+    ) {
       setIsGalleryVisible(true);
-    } else if (photoPermissionStatus === 'denied' || photoPermissionStatus === 'blocked') {
+    } else if (
+      photoPermissionStatus === 'denied' ||
+      photoPermissionStatus === 'blocked'
+    ) {
       Alert.alert('사진 접근 권한이 필요합니다', '설정에서 허용해주세요', [
         { text: '취소', style: 'cancel' },
         { text: '설정 열기', onPress: openAppSettings },
       ]);
     } else {
       const requestResult = await requestGalleryPermissions();
-      if (requestResult === 'granted' || requestResult === 'limited') setIsGalleryVisible(true);
+      if (requestResult === 'granted' || requestResult === 'limited')
+        setIsGalleryVisible(true);
       else if (requestResult === 'denied' || requestResult === 'blocked') {
         Alert.alert('사진 접근 권한이 필요합니다', '설정에서 허용해주세요', [
           { text: '취소', style: 'cancel' },
@@ -422,7 +429,6 @@ export default function CameraPreview() {
       </View>
       <CameraActionBar
         onTakePhoto={onTakePhoto}
-        thumbnailUri={thumbnailUri}
         openGallery={openGallery}
         onStickerPress={openStickerSheet}
       />
