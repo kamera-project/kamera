35d0bb8f25a6930dbc30b9a32de664b9d8d83d2f
janei <67265050+rio-sung-ks@users.noreply.github.com>
2025-06-17 15:33:31 +0900

    Refactor: make a overlay switch on as a default (close #56)
    refactor: make a overlay switch on as a default 
    style: re-arrange a reset button

--- src/utils/camera/takePhoto.js
+++ src/utils/camera/takePhoto.js
@@ -2,7 +2,7 @@ import { CameraRoll } from '@react-native-camera-roll/camera-roll';
 import { useCameraStore } from '../../store/useCameraStore';
 
 export const handleTakePhoto = async (cameraRef, flash) => {
-  const { isFeatureOn } = useCameraStore.getState();
+  const { isOverlaySwitchOn } = useCameraStore.getState();
 
   const photo = await cameraRef.current.takePhoto({
     flash,
@@ -16,7 +16,7 @@ export const handleTakePhoto = async (cameraRef, flash) => {
   await CameraRoll.saveAsset(`file://${photo.path}`, {
     type: 'photo',
   });
-  if (isFeatureOn) {
+  if (isOverlaySwitchOn) {
     return photo.path;
   }
 };
